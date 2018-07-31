de86c700c1b755b4b017a3d9500c44271e8fa11a
Chaiyapong Lapliengtrakul <chaiyapong@Chaiyapongs-MacBook-Pro.local>
2018-07-31 10:17:12 +0700

    Remove mock hero

--- src/app/di/container.ts
+++ src/app/di/container.ts
@@ -2,13 +2,11 @@ import { Container } from 'inversify';
 import SERVICES from '@/app/di/services';
 import getDecorators from 'inversify-inject-decorators';
 import { HeroService } from '@/app/heroes/hero-service';
-import { MockHeroes } from '@/app/heroes/mock-heroes';
 import { HttpService } from '@/app/common/http-service';
 import { HeroState } from '@/app/heroes/hero-state';
 
 const container = new Container();
 container.bind<HttpService>(SERVICES.HttpService).to(HttpService);
-container.bind<MockHeroes>(SERVICES.MockHeroes).to(MockHeroes);
 container.bind<HeroService>(SERVICES.HeroService).to(HeroService);
 container.bind<HeroState>(SERVICES.HeroState).to(HeroState);
 
